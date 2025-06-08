a57075b5d758837fb6d46cf17c2991ecc4f67793
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-06-08 20:18:48 -0300

    first commit

---
+++ filme/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from filme.models import Usuario
+
+class FormHomepage(forms.Form):
+    email = forms.EmailField(label='')
+
+class CriarContaForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta(UserCreationForm):
+        model = Usuario
+        fields = ('username', 'email', 'password1', 'password2')
+
