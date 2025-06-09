c15a189e797eb9f9b059aa77451812adeca9124f
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-06-08 22:47:39 -0300

    Adicionando log no wsgi.py

--- hashflix/wsgi.py
+++ hashflix/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hashflix.settings")
 
 application = get_wsgi_application()
+print("Estou na parte do wsgi do meu site!")
