e2f6aa923461e06b58d38a050fe08d5f660027d4
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-06-08 21:27:07 -0300

    Ativando o DEBUG para analisar o erro

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7y1t4zm@hds*=srh+!vr76%fc7u1q3-#d1ti#le@_wbx1hu%yd"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://projetohashflixdjango-production-1a3f.up.railway.app/', 'localhost', '127.0.0.1']
 
 # Application definition
 
