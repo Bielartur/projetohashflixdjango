155130368ef740b00e95c396e1cdf80c15abb631
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-06-10 21:18:31 -0300

    Tentanto concertar a media com DEBUG=False

--- hashflix/urls.py
+++ hashflix/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('filme.urls', namespace='filme'))
+    path("", include('filme.urls', namespace='filme')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
